f759e8667c6b22f183981ad083b0bb04e98171c7
Ali Hassan <73987102+dooomdev@users.noreply.github.com>
2021-06-30 09:45:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '////////////////'
+app.config['SECRET_KEY'] =  os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///todo.db').replace("://"
                                                                                                            , "ql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
